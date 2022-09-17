b8602386147a9f9e7b9821db0f38b26b4fa1b251
RHamudi <ramonramos.silva16@gmail.com>
2022-09-16 21:38:49 -0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
+import { useState } from "react";
 import * as C from "./App.styles";
+import { ListItem } from "./Components/ListItem";
+import { Item } from "./Types/item";
 
 function App() {
+  const [list, setList] = useState<Item[]>([
+    { id: 1, name: "Comprar o Pão na padaria", done: false },
+    { id: 2, name: "Comprar o bolo na padaria", done: false },
+  ]);
+
   return (
     <C.Container>
       <C.Area>
         <C.Header>Lista De Tarefas</C.Header>
+
+        {list.map((item, index) => (
+          <ListItem key={index} item={item} />
+        ))}
       </C.Area>
     </C.Container>
   );
